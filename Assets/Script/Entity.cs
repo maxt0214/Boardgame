e5457e3e391379b32eb479ea7f4766f04f39e02e
XIAOTMA <49177628+maxt0214@users.noreply.github.com>
2022-02-18 20:41:02 -0500

    Added Entity

---
+++ Assets/Script/Entity.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// All Gameobjects that need to be networked must be inhereted from Entity
+/// </summary>
+public class Entity : MonoBehaviour
+{
+    private int id;
+}
